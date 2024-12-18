02fa5af51a12f33e8d32a9d576566377511c18f5
criss <cristian.arciniega071@ist17dejulio.edu.ec>
2024-12-18 12:53:52 -0500

    Trinomio cuadrado perfecto

--- ejercicioExpress/index.js
+++ ejercicioExpress/index.js
@@ -33,7 +33,7 @@ app.get('/suma', (req, res) => {
 
 // consumo de servicio 4
 app.get('/suma/:n1', (req, res) => {
-    let n1 = parseInt (req.params.n1); //obteniendo el dato n1
+    let n1 = parseInt(req.params.n1); //obteniendo el dato n1
     let n2 = 9;
     let suma = n1 + n2;
     res.send('El resultado de la suma es: ' + suma);
@@ -42,8 +42,8 @@ app.get('/suma/:n1', (req, res) => {
 //consumo 5
 // Área y perímetro del rombo
 app.get('/figura/rombo/:d1/:d2', (req, res) => {
-    const d1 = parseFloat(req.params.d1); 
-    const d2 = parseFloat(req.params.d2); 
+    const d1 = parseFloat(req.params.d1);
+    const d2 = parseFloat(req.params.d2);
     const area = (d1 * d2) / 2;
     const perimetro = 4 * Math.sqrt((d1 / 2) ** 2 + (d2 / 2) ** 2);
 
@@ -62,12 +62,29 @@ app.get('/figura/romboide/:base/:altura', (req, res) => {
 
 // Área y perímetro del deltoide
 app.get('/figura/deltoide/:d1/:d2', (req, res) => {
-    const d1 = parseFloat(req.params.d1); 
-    const d2 = parseFloat(req.params.d2); 
+    const d1 = parseFloat(req.params.d1);
+    const d2 = parseFloat(req.params.d2);
     const area = (d1 * d2) / 2;
     const perimetro = 2 * (d1 + d2);
 
     res.send(`Deltoide: Área = ${area}, Perímetro = ${perimetro}`);
+});
+
+//consumo 6
+app.get('/trinomio/:a/:b/:tipo', (req, res) => {
+    const a = parseFloat(req.params.a);
+    const b = parseFloat(req.params.b);
+    const tipo = req.params.tipo; // Tipo: suma o resta
+
+    let resultado;
+    if (tipo === 'suma') {
+        resultado = (a ** 2) + (2 * a * b) + (b ** 2); // (a + b)^2
+    } else if (tipo === 'resta') {
+        resultado = (a ** 2) - (2 * a * b) + (b ** 2); // (a - b)^2
+    } else {
+        return res.status(400).send('Tipo inválido. Usa "suma" o "resta".');
+    }
+    res.send(`El resultado de la operación es: ${resultado}`);
 });
 
 //llamada al puerto por defecto de node 3000
